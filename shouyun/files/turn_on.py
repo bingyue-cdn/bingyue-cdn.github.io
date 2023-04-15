12e7db06e38803cf459df960469745f4680a5907
bingyue-cdn <117450861+bingyue-cdn@users.noreply.github.com>
2023-04-15 11:02:51 +0800

    Update turn_on.py

--- shouyun/files/turn_on.py
+++ shouyun/files/turn_on.py
@@ -15,12 +15,5 @@ if a["ver"] == verison:
     logger.success("暂无新版本更新")
 else:
     logger.warning(f"检测到新版本:{a['ver']}\n更新日志\n{a['log']}")
-    logger.success(f"正在为你下载更新")
-    for i in a["files"]:
-        n = httpx.get(url=f"https://{mirr}/shouyun/files/{i}").text
-        with open(f"plugins/{i}") as f:
-            f.write(n)
-            f.close()
-        logger.success("正在下载最新版")
 
 logger.success(" ╰(*°▽*)╯ 祝你使用愉快")
